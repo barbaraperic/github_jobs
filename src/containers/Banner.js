839a8dd0e837e0bd5f0001da747e83d1f66b6f6d
Barbara Peric <b.peric10@gmail.com>
2021-05-23 20:13:54 +0200

    FEAT: banner component

---
+++ src/containers/Banner.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Banner = () => {
+  return (
+    <>
+    <Title><strong>GitHub</strong> jobs</Title>
+    <Wrapper>
+
+    </Wrapper>
+    </>
+  )
+}
+
+const Wrapper = styled.div`
+  height: 138px;
+  width: 100%;
+  border-radius: 8px;
+  margin-bottom: 42px;
+  margin-top: 32px;
+  background-image: url("https://images.unsplash.com/photo-1541953445540-3e450e21a945?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80")
+`
+
+const Title = styled.h1`
+  margin-top: 32px;
+`
+
+export default Banner
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import Banner from "../containers/Banner"
 import Cards from '../containers/Cards'
 
 const Home = () => {
   return (
     <>
+      <Banner />
       <Cards />
     </>
   )
