85e16bae6f29bb3a3b7f35d6234fbcc36b4532ac
Barbara Peric <b.peric10@gmail.com>
2021-05-23 12:20:39 +0200

    colour and typography

---
+++ src/utils/colours.js
@@ -0,0 +1,13 @@
+export const blue = {
+  100: "#334680",
+  200: "#1E86FF",
+};
+
+export const neutral = {
+  100: "#ffffff",
+  200: "#F6F7FB",
+  300: "#B9BDCF",
+  400: "#282538",
+  600: "#000000"
+};
+
---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+export const primaryFont = '"Roboto", sans-serif';
+export const secondaryFont = '"Poppins", sans-serif';
+
+
+export const typeScale = {
+  header1: 24 / 16 + "rem",
+  header2: 18 / 16 + "rem",
+  paragraph: "1rem",
+  helperText:  14 / 16 + "rem",
+  smallText: 12 / 16 + "rem"
+};
